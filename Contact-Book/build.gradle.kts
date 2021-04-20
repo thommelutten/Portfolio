79d8484b22b85b3782e55ad2afc273a3f8bcc792
Thomas <thomas.m.elbo@gmail.com>
2021-04-20 19:49:49 +0200

    Added jacoco

--- Contact-Book/build.gradle.kts
+++ Contact-Book/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 plugins {
     kotlin("jvm") version "1.4.10"
     id("org.jetbrains.kotlin.kapt") version "1.4.32"
+    jacoco
     application
 
 }
@@ -23,13 +24,27 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.12.0")
     implementation("com.squareup.moshi:moshi-kotlin-codegen:1.12.0")
     implementation("com.squareup.moshi:moshi:1.12.0")
+    implementation("org.jacoco:org.jacoco.core:0.8.4")
     kapt("com.squareup.moshi:moshi-kotlin-codegen:1.12.0")
     testImplementation(kotlin("test-junit"))
 
 }
 
+
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.isEnabled = true
+        csv.isEnabled = false
+        html.destination = layout.buildDirectory.dir("jacocoHtml").get().asFile
+    }
 }
 
 tasks.withType<KotlinCompile> {
